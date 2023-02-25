9b894c835ef376b780865dd046a925522b33016a
YvesCousteau <hugohenrotte@hotmail.com>
2023-02-25 16:16:58 +0100

    ko

--- services/sound/client_master.py
+++ services/sound/client_master.py
@@ -1,25 +1,15 @@
 import socket
 import sys
 import json 
-import threading, wave, pyaudio, time, queue
+import threading, wave, pyaudio, time
 
 port = 9633
 bufferSize = 65536
 ip = sys.argv[1]
-q = queue.Queue(maxsize=2000)
 chunk = 1024
+wf = wave.open("../../assets/file_example.wav")
 p = pyaudio.PyAudio()
 
-def getAudioData():
-    while True:
-        frame,_= sock.recvfrom(bufferSize)
-        q.put(frame)
-        print('Queue size...',q.qsize())
-
-msgFromClient = {"value":sys.argv[2]}
-bytesToSend = json.dumps(msgFromClient).encode()
-
-serverAddressPort = (sys.argv[1], port)
 # Create a socket at client side
 try:
     sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -27,29 +17,31 @@ try:
 except socket.error as err:
     print('Socket error because of %s' %(err))
 
+print("channels ",wf.getnchannels())
+print("format ",wf.getsampwidth())
+print("rate ",wf.getframerate())
 
 stream = p.open(
-    format=p.get_format_from_width(2),
-    channels=1,
-    rate=22050,
-    output=True,
+    format=p.get_format_from_width(wf.getsampwidth()),
+    channels=wf.getnchannels(),
+    rate=wf.getframerate(),
+    input=True,
     frames_per_buffer=chunk
 )
-# create socket
-message = b'Hello'
-sock.sendto(message,(ip,port))
-socket_address = (ip,port)
 
-t1 = threading.Thread(target=getAudioData, args=())
-t1.start()
-time.sleep(1)
+sock.sendto(b'SEXE', (sys.argv[1], port))
 
-print('Now Playing...')
+data = None
+sample_rate = wf.getframerate()
 while True:
-    frame = q.get()
-    stream.write(frame)
+    # msg,address = sock.recvfrom(bufferSize)
+    # print('Message from Client >> {}'.format(msg))
+    # print('Client IP Address:{}'.format(address))
+    while True:
+        data = wf.readframes(chunk)
+        sock.sendto(data,(sys.argv[1], port))
+        time.sleep(0.95*chunk/sample_rate)
+    
+    sock.sendto(msg,address)
 
 sock.close()
-
-
-
