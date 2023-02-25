d7f2ea9d5dbb3e4442cfb70ac851b5194066e712
YvesCousteau <hugohenrotte@hotmail.com>
2023-02-25 14:30:50 +0100

    la c cool on fait du son lol

---
+++ services/sound/client_master.py
@@ -0,0 +1,53 @@
+import socket
+import sys
+import json 
+import threading, wave, pyaudio, time, queue
+
+port = 9633
+bufferSize = 65536
+ip = sys.argv[1]
+chunk = 10*1024
+p = pyaudio.PyAudio()
+
+
+msgFromClient = {"value":sys.argv[2]}
+bytesToSend = json.dumps(msgFromClient).encode()
+
+serverAddressPort = (sys.argv[1], port)
+# Create a socket at client side
+try:
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,bufferSize)
+except socket.error as err:
+    print('Socket error because of %s' %(err))
+
+
+stream = p.open(
+    format=p.get_format_from_width(2),
+    channels=2,
+    rate=44100,
+    output=True,
+    frames_per_buffer=chunk
+)
+# create socket
+message = b'Hello'
+sock.sendto(message,(ip,port))
+socket_address = (ip,port)
+
+t1 = threading.Thread(target=getAudioData, args=())
+t1.start()
+time.sleep(5)
+
+print('Now Playing...')
+while True:
+    frame = q.get()
+    stream.write(frame)
+
+sock.close()
+
+def getAudioData():
+    while True:
+        frame,_= sock.recvfrom(bufferSize)
+        q.put(frame)
+        print('Queue size...',q.qsize())
+
---
+++ services/sound/server_slave.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+import json
+import os
+import threading, wave, pyaudio, time
+
+port = 9633
+bufferSize = 65536
+ip = sys.argv[1]
+chunk = 10*1024
+wf = wave.open("../../assets/file_example.wav")
+p = pyaudio.PyAudio()
+
+# Create a datagram socket
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,bufferSize)
+print("Socket created ...")
+# Bind to address and ip
+sock.bind((ip, port))
+print("Server up and listening on : "+ip+" / "+str(port))
+print('server listening at',(ip, (port)),wf.getframerate())
+stream = p.open(
+    format=p.get_format_from_width(wf.getsampwidth()),
+    channels=wf.getnchannels(),
+    rate=wf.getframerate(),
+    input=True,
+    frames_per_buffer=chunk
+)
+data = None
+sample_rate = wf.getframerate()
+# Listen for incoming datagrams
+msg = str.encode("Hello Client!")
+while True:
+        msg,address = sock.recvfrom(bufferSize)
+        print('Message from Client >> {}'.format(msg))
+        print('Client IP Address:{}'.format(address))
+        while True:
+            data = wf.readframes(chunk)
+            sock.sendto(data,address)
+            time.sleep(0.8*chunk/sample_rate)
+        
+        sock.sendto(msg,address)
